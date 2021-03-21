9d107e80bd765c298aa7d42f8c5d86edc7fdbb46
adeniyekehinde0@gmail.com <adeniyekehinde0@gmail.com>
2021-03-21 15:49:19 +0100

    practice

---
+++ 2nd_Week Assessment.js
@@ -0,0 +1,28 @@
+/*Exercises on introduction to programming
+Write a program that reads in (from standard input) a price of an item of clothing in dollars and then writes out to standard output how much the price would be if that item was at a 10% discount. 
+Write a program that reads in two numbers from standard input: the price of sneakers before discount, and discounted price. The program should output the percentage of the sneakers that were discounted.
+Write a program that reads in (from standard input) the length of a movie in hours and minutes. The program should calculate and write out (to standard output) the length of the movie in minutes.
+Write a program that reads in (from standard input) the length of a movie in hours, minutes, and seconds. The program should calculate and write out (to standard output) the length of the movie in seconds.
+Write a program that reads in one natural number (let us call it n) and then prints out squares of the first n natural numbers.
+Write a program that reads in two numbers (from standard input) and then writes out the value of the smaller ones.
+Write a program that reads in three different numbers and then writes out the value of the smallest one.
+Write a program that reads in one natural number and then writes out a message telling whether the given number has exactly two digits.
+Let us play with corporate taxes. Write a program that reads in two numbers: the annual income and annual expenses of a company, expressed in dollars. The program has to write out the amount of tax that the company has to pay; let us call it the profit tax. If the expenses are greater than the income, then the profit tax equals 0 dollars. Otherwise, let the profit tax be equal to 20% of the profits, where profits equal the difference of income and expenses.
+Write a program that reads in a number n. Then it should write out the first power of number 10 that is greater than number n. The powers of number 10 are 1, 10, 100, 1000, 10000, and so on.
+N = 50, a = 
+for(i = 1; i < power.length; i++){
+If (p10 > n){
+Return n}
+}
+Write a program that reads in a number n. Then it should write out all the powers of number 10 that are smaller than the number n.
+Write a function named Sub which takes two arguments of type double, and returns the result of subtracting the second one from the first one. Use this function to calculate 3.2 – 8 and write out the result. The result should be -4.8.
+ Write a JavaScript program to get a list of elements that exist in both arrays.
+14. Write a JavaScript program to remove specified elements from the left of a   given array of elements.
+15. Write a JavaScript program to filter out the element(s) of a given array, that has one of the specified values.
+16. Write a JavaScript program to get the sum of a given array, after mapping each element to a value using the provided function.
+17. Write a JavaScript program to get a random number in the specified range.
+18.  Write a JavaScript program to round a number to a specified amount of digits. 
+19. Write a JavaScript program to capitalize the first letter of every word in a string. 
+20.  Write a JavaScript program to get all the elements of an array except the last one. */
+ 
+
---
+++ index.js
@@ -0,0 +1,44 @@
+/*
+1.comment are line of code that are ignored used to create note 
+
+
+2. Data types :
+undefined, null, boolean, string,symbol,number, and object
+
+3. Declaring a variable with a number and a string*/
+
+
+var a;
+var b = 2;
+
+console.log(a)
+
+// the difference between VAR,LET AND CONST
+
+/*
+Var can be changed within the scope*/
+
+var myName ="Beau";
+
+myName = 8;
+
+console.log(myName);
+
+var a;
+var b = 2; //declaring a and assigning to 2
+ 
+a = 7;
+b = a;
+console.log (a);
+
+//LET can be used within the scope of where it been declared
+
+let ourName = "Kehinde"
+
+//CONST a variable that can never changed e.g
+const pi = 3.14 */
+
+
+
+
+
